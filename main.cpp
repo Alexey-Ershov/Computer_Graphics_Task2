b81a624bbde9ed18f87b396d8303f09bd2fe3fcc
Alexey-Ershov <ershov_ae@mail.ru>
2019-05-01 21:09:24 +0300

    Draw simple rectangle

--- main.cpp
+++ main.cpp
@@ -7,7 +7,20 @@
 #include <GLFW/glfw3.h>
 #include <random>
 
-static const GLsizei WIDTH = 640, HEIGHT = 480; //размеры окна
+static const GLsizei WIDTH = 1024, HEIGHT = 768; //размеры окна
+
+
+void key_callback(GLFWwindow* window,
+                  int key,
+                  int scancode,
+                  int action,
+                  int mode)
+{
+    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+    // и приложение после этого закроется
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int initGL()
 {
@@ -27,10 +40,12 @@ int initGL()
 	return 0;
 }
 
+
 int main(int argc, char** argv)
 {
-	if(!glfwInit())
-    return -1;
+	if (!glfwInit()) {
+        return -1;
+    }
 
 	//запрашиваем контекст opengl версии 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
@@ -38,9 +53,13 @@ int main(int argc, char** argv)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 
 
-  GLFWwindow*  window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL basic sample", nullptr, nullptr);
-	if (window == nullptr)
-	{
+    GLFWwindow*  window = glfwCreateWindow(WIDTH,
+                                           HEIGHT,
+                                           "SMIERTIELNAJA BITWA",
+                                           nullptr,
+                                           nullptr);
+
+	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -49,85 +68,125 @@ int main(int argc, char** argv)
 	glfwMakeContextCurrent(window); 
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
-	if(initGL() != 0) 
-		return -1;
+    glfwSetKeyCallback(window, key_callback);
+
+	if (initGL() != 0) {
+	   return -1;
+    }
 	
-  //Reset any OpenGL errors which could be present for some reason
+    //Reset any OpenGL errors which could be present for some reason
 	GLenum gl_error = glGetError();
-	while (gl_error != GL_NO_ERROR)
+	while (gl_error != GL_NO_ERROR){
 		gl_error = glGetError();
+    }
 
 	//создание шейдерной программы из двух файлов с исходниками шейдеров
 	//используется класс-обертка ShaderProgram
 	std::unordered_map<GLenum, std::string> shaders;
-	shaders[GL_VERTEX_SHADER]   = "vertex.glsl";
+	shaders[GL_VERTEX_SHADER] = "vertex.glsl";
 	shaders[GL_FRAGMENT_SHADER] = "fragment.glsl";
-	ShaderProgram program(shaders); GL_CHECK_ERRORS;
+	ShaderProgram program(shaders);
+    GL_CHECK_ERRORS;
 
-  glfwSwapInterval(1); // force 60 frames per second
+    glfwSwapInterval(1); // force 60 frames per second
+
+    //Создаем и загружаем геометрию поверхности
+    GLuint g_vertexBufferObject;
+    GLuint g_vertexArrayObject;
+    GLuint EBO;
   
-  //Создаем и загружаем геометрию поверхности
-  //
-  GLuint g_vertexBufferObject;
-  GLuint g_vertexArrayObject;
-  {
     float trianglePos[] =
     {
-      -0.5f, -0.5f,
-      0.5f, -0.5f,
-      0.0f, +0.5f,
+        0.5f,  0.5f,  0.0f,  // Верхний правый угол
+        0.5f, -0.5f,  0.0f,  // Нижний правый угол
+       -0.5f, -0.5f,  0.0f,  // Нижний левый угол
+       -0.5f,  0.5f,  0.0f   // Верхний левый угол
+    };
+
+    GLuint indices[] =
+    {
+        0, 1, 3, // Первый треугольник
+        1, 2, 3  // Второй треугольник
     };
 
     g_vertexBufferObject = 0;
-    GLuint vertexLocation = 0; // simple layout, assume have only positions at location = 0
+    GLuint vertexLocation = 0;
 
-    glGenBuffers(1, &g_vertexBufferObject);                                                        GL_CHECK_ERRORS;
-    glBindBuffer(GL_ARRAY_BUFFER, g_vertexBufferObject);                                           GL_CHECK_ERRORS;
-    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(GLfloat), (GLfloat*)trianglePos, GL_STATIC_DRAW); GL_CHECK_ERRORS;
+    glGenVertexArrays(1, &g_vertexArrayObject);
 
-    glGenVertexArrays(1, &g_vertexArrayObject);                                                    GL_CHECK_ERRORS;
-    glBindVertexArray(g_vertexArrayObject);                                                        GL_CHECK_ERRORS;
+    glGenBuffers(1, &g_vertexBufferObject);
+    GL_CHECK_ERRORS;
+    
+    glGenBuffers(1, &EBO);
+    GL_CHECK_ERRORS;
+    
+    glBindVertexArray(g_vertexArrayObject);
 
-    glBindBuffer(GL_ARRAY_BUFFER, g_vertexBufferObject);                                           GL_CHECK_ERRORS;
-    glEnableVertexAttribArray(vertexLocation);                                                     GL_CHECK_ERRORS;
-    glVertexAttribPointer(vertexLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);                            GL_CHECK_ERRORS;
+    glBindBuffer(GL_ARRAY_BUFFER, g_vertexBufferObject);
+    GL_CHECK_ERRORS;
+
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(trianglePos),
+                 (GLfloat *) trianglePos,
+                 GL_STATIC_DRAW);
+    GL_CHECK_ERRORS;
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    GL_CHECK_ERRORS;
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                 sizeof(indices),
+                 (GLfloat *) indices,
+                 GL_STATIC_DRAW);
+    GL_CHECK_ERRORS;
+
+    glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    GL_CHECK_ERRORS;
+
+    glEnableVertexAttribArray(vertexLocation);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
-  }
+    GL_CHECK_ERRORS;
 
-	//цикл обработки сообщений и отрисовки сцены каждый кадр
-	while (!glfwWindowShouldClose(window))
-	{
+	// Game loop.
+	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
 		//очищаем экран каждый кадр
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);               GL_CHECK_ERRORS;
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); GL_CHECK_ERRORS;
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        GL_CHECK_ERRORS;
+		
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GL_CHECK_ERRORS;
 
-    program.StartUseShader();                           GL_CHECK_ERRORS;
+        glClearColor(0.0f, 0.169f, 0.212f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT |
+                GL_DEPTH_BUFFER_BIT |
+                GL_STENCIL_BUFFER_BIT);
 
+        program.StartUseShader();
+        GL_CHECK_ERRORS;
 
-    // очистка и заполнение экрана цветом
-    //
-    glViewport  (0, 0, WIDTH, HEIGHT);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear     (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        glBindVertexArray(g_vertexArrayObject);
+        GL_CHECK_ERRORS;
 
-    // draw call
-    //
-    glBindVertexArray(g_vertexArrayObject); GL_CHECK_ERRORS;
-    glDrawArrays(GL_TRIANGLES, 0, 3);       GL_CHECK_ERRORS;  // The last parameter of glDrawArrays is equal to VS invocations
-    
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        /*GL_CHECK_ERRORS;  // The last parameter of glDrawArrays is equal to
+                          // VS invocations*/
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    program.StopUseShader();
+        glBindVertexArray(0);
 
-		glfwSwapBuffers(window); 
+        program.StopUseShader();
+
+        glfwSwapBuffers(window); 
 	}
 
-	//очищаем vboи vao перед закрытием программы
-  //
 	glDeleteVertexArrays(1, &g_vertexArrayObject);
-  glDeleteBuffers(1,      &g_vertexBufferObject);
+    glDeleteBuffers(1, &g_vertexBufferObject);
+    glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,5 +4,5 @@ out vec4 color;
 
 void main()
 {
-  color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+  color = vec4(0.71f, 0.09f, 0.03f, 1.0f);
 }
