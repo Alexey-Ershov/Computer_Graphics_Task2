49bdb60ea943284644d84045c4fb1efffd1f27c6
Alexey-Ershov <ershov_ae@mail.ru>
2019-05-04 12:17:30 +0300

    Add scope

--- main.cpp
+++ main.cpp
@@ -11,14 +11,20 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #include <random>
 #include <string>
 #include <vector>
+#include <map>
 
 #include <ctime>
 #include <cmath>
 
+
+/*float iks;
+float igrec;*/
 
 struct ModelAttributes
 {
@@ -26,6 +32,19 @@ struct ModelAttributes
     float x;
     float y;
 };
+
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character
+{
+    GLuint TextureID;   // ID handle of the glyph texture
+    glm::ivec2 Size;    // Size of glyph
+    glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+    GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
+GLuint VAO;
+GLuint VBO;
 
 static const GLsizei WIDTH = 1120;
 static const GLsizei HEIGHT = 840;
@@ -43,8 +62,8 @@ float lastFrame = 0.0f;
 ShaderProgram program;
 ShaderProgram model_program;
 ShaderProgram skybox_program;
+ShaderProgram text_program;
 unsigned int cubemapTexture;
-GLuint VAO;
 unsigned int skyboxVAO;
 float current_frame;
 GLuint scope_texture;
@@ -60,18 +79,22 @@ void processInput(GLFWwindow *window)
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
         camera.ProcessKeyboard(FORWARD, deltaTime);
+        // igrec++;
     }
 
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
         camera.ProcessKeyboard(BACKWARD, deltaTime);
+        // igrec--;
     }
 
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         camera.ProcessKeyboard(LEFT, deltaTime);
+        // iks--;
     }
 
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
         camera.ProcessKeyboard(RIGHT, deltaTime);
+        // iks++;
     }
 }
 
@@ -245,6 +268,61 @@ void draw_skybox()
     glBindVertexArray(0);
 }*/
 
+void RenderText(ShaderProgram &program,
+                std::string text,
+                GLfloat x,
+                GLfloat y,
+                GLfloat scale,
+                glm::vec3 color)
+{
+    // Activate corresponding render state  
+    program.StartUseShader();
+    glUniform3f(glGetUniformLocation(
+                program.GetProgram(), "textColor"),
+                color.x,
+                color.y,
+                color.z);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    // Iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++) 
+    {
+        Character ch = Characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // Update VBO for each character
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },            
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }           
+        };
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 int initGL()
 {
 	int res = 0;
@@ -317,21 +395,106 @@ int main(int argc, char** argv)
 	program = ShaderProgram(shaders);
     GL_CHECK_ERRORS;*/
 
-    //создание шейдерной программы из двух файлов с исходниками шейдеров
-    //используется класс-обертка ShaderProgram
     std::unordered_map<GLenum, std::string> skybox_shaders;
     skybox_shaders[GL_VERTEX_SHADER] = "skybox_vertex.glsl";
     skybox_shaders[GL_FRAGMENT_SHADER] = "skybox_fragment.glsl";
     skybox_program = ShaderProgram(skybox_shaders);
     GL_CHECK_ERRORS;
 
-    //создание шейдерной программы из двух файлов с исходниками шейдеров
-    //используется класс-обертка ShaderProgram
     std::unordered_map<GLenum, std::string> model_shaders;
     model_shaders[GL_VERTEX_SHADER] = "model_vertex.glsl";
     model_shaders[GL_FRAGMENT_SHADER] = "model_fragment.glsl";
     model_program = ShaderProgram(model_shaders);
     GL_CHECK_ERRORS;
+
+    std::unordered_map<GLenum, std::string> text_shaders;
+    text_shaders[GL_VERTEX_SHADER] = "text_vertex.glsl";
+    text_shaders[GL_FRAGMENT_SHADER] = "text_fragment.glsl";
+    text_program = ShaderProgram(text_shaders);
+    GL_CHECK_ERRORS;
+
+    glm::mat4 projection = glm::ortho(0.0f,
+                                      static_cast<GLfloat>(WIDTH),
+                                      0.0f,
+                                      static_cast<GLfloat>(HEIGHT));
+    text_program.StartUseShader();
+    glUniformMatrix4fv(glGetUniformLocation(
+                       text_program.GetProgram(), "projection"),
+                       1,
+                       GL_FALSE,
+                       glm::value_ptr(projection));
+
+    // FreeType
+    FT_Library ft;
+    // All functions return a value different than 0 whenever an error occurred
+    if (FT_Init_FreeType(&ft))
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+    // Load font as face
+    FT_Face face;
+    if (FT_New_Face(ft, "../resources/fonts/arial.ttf", 0, &face))
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+    // Set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    // Disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); 
+
+    // Load first 128 characters of ASCII set
+    for (GLubyte c = 0; c < 128; c++)
+    {
+        // Load character glyph 
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // Generate texture
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // Set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // Now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            (GLuint) face->glyph->advance.x
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // Destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+    
+    // Configure VAO/VBO for texture quads
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // glfwSwapInterval(1); // force 60 frames per second
 
@@ -539,6 +702,13 @@ int main(int argc, char** argv)
 
         draw_skybox();
 
+        RenderText(text_program,
+                   "+",
+                   545.0f,
+                   400.0f,
+                   0.5f,
+                   glm::vec3(1.0f, 1.0f, 1.0f));
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -551,6 +721,9 @@ int main(int argc, char** argv)
     /*glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);*/
+
+    /*std::cout << "iks = " << iks << std::endl;
+    std::cout << "igrec = " << igrec << std::endl;*/
 
     glfwTerminate();
     return 0;
